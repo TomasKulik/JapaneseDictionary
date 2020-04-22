1dfcd2d78a1afb58639c82c0512ef1a92dd50a0d
Karel <Karel4ever@gmail.com>
2020-04-22 18:06:07 +0200

    Parsing JSON data and showing it in Listview

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -7,6 +10,24 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   final TextEditingController _searchController = TextEditingController();
+  bool _isLoading = true;
+
+  var words;
+
+  _fetchData(String find) async {
+    final url = 'https://jisho.org/api/v1/search/words?keyword=$find';
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final map = json.decode(response.body);
+      final wordsJson = map['data'];
+
+      setState(() {
+        words = wordsJson;
+        _isLoading = false;
+      });
+    }
+  }
 
   _clearSearch() {
     WidgetsBinding.instance
@@ -34,8 +55,30 @@ class _SearchScreenState extends State<SearchScreen> {
               onPressed: _clearSearch,
             ),
           ),
-          onSubmitted: (input) => print(input),
+          onSubmitted: (input) {
+            setState(() {
+              _isLoading = true;
+            });
+            _fetchData(input);
+          },
         ),
+      ),
+      body: Center(
+        child: _isLoading
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: words != null ? words.length : 0,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: words[index]['japanese'][0]['word'] == ''
+                        ? Text(words[index]['japanese'][0]['word'])
+                        : Text(words[index]['japanese'][0]['reading']),
+                    subtitle: Text(words[index]['japanese'][0]['reading']),
+                    trailing: Text(
+                        words[index]['senses'][0]['english_definitions'][0]),
+                  );
+                },
+              ),
       ),
     );
   }
